96d9ba4927439e2d45aa3549fe8176da87ce076c
Robusr <Robusr@outlook.com>
2026-08-27 21:04:57 +0800

    feat(sdk): export replay reader

---
+++ sdk/output/__init__.py
@@ -0,0 +1,17 @@
+from sdk.output.frame import (
+    BBox2D,
+    BBox3D,
+    Detection,
+    Track,
+    TrafficSign,
+    PerceptionFrame,
+)
+
+__all__ = [
+    "BBox2D",
+    "BBox3D",
+    "Detection",
+    "Track",
+    "TrafficSign",
+    "PerceptionFrame",
+]
